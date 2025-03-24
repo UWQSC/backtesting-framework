4ba2ea262dd373336f43e37551fb0fd563e8f2da
TheFJcurve <sargunbhatti27@gmail.com>
2025-03-23 21:04:04 -0400

    Adding an example for Simple Moving Average
    topic/#15
    
    - Currently using yfinance to get the data

--- uwqsc_backtesting_framework/interfaces/backtesting_framework_interface.py
+++ uwqsc_backtesting_framework/interfaces/backtesting_framework_interface.py
@@ -2,7 +2,10 @@
 Interface for a Backtesting Framework Algorithm
 """
 
-from abc import ABC
+from abc import ABC, abstractmethod
+from typing import List
+
+import pandas as pd
 
 from uwqsc_algorithmic_trading.interfaces.algorithms.algorithm_interface import IAlgorithm
 
@@ -14,6 +17,33 @@ class IBacktestingFramework(ABC):
 
     def __init__(self,
                  name: str,
+                 capital: int,
+                 tickers: List[str],
                  algorithm: IAlgorithm):
-        self._name = name
+        self.name = name
+        self.capital = capital
+        self.tickers = tickers
         self.algorithm = algorithm
+        self.data = None
+
+    @abstractmethod
+    def load_data(self):
+        """
+        Loads the data for running the algorithm
+        """
+
+    def run(self):
+        """
+        Runs the trades over the loaded data
+        """
+
+        self.load_data()
+
+        for index, current_data in self.data.iterrows():
+            current_data = pd.DataFrame(current_data).T
+            cost_per_ticker = self.algorithm.execute_trade(self.capital, current_data)
+
+            for ticker in self.tickers:
+                self.capital += cost_per_ticker[ticker]
+
+        print(f"[{self.name}] Final capital with: {self.capital}")
